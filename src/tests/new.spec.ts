3fb1664d4a89bcd37e25ab96839ac864ed129f16
Mitul das <mituldas751@gmail.com>
2024-11-27 16:23:46 +0600

    new page Add

--- src/tests/new.spec.ts
+++ src/tests/new.spec.ts
@@ -19,14 +19,7 @@ test("hover", async () => {
   await page.hover("(//a[contains(@class,'elementor-item elementor-item-active')])[1]");
 });
 
-// Click test
-
 test("Click", async () => {
   await page.click('//header/div[1]/section[1]/div[1]/div[3]/div[1]/div[1]/div[1]/nav[1]/ul[1]/li[3]/ul[1]/li[1]/a[1]');
   await page.waitForTimeout(2000); 
 });
-
-// // Cleanup after each test
-// test.afterEach(async () => {
-//   await browser.close();
-// });
